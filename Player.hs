b862d2fdff7ed5861c14bf2587cfa71eb6da9785
Randall Britten <randall.britten@gmail.com>
2016-05-09 02:05:22 +1200

    Huge increase in constant bet

--- Player.hs
+++ Player.hs
@@ -4,10 +4,10 @@ import Data.Aeson (Object)
 
 
 version :: String
-version = "Haskell - boring constant betting player"
+version = "Haskell - crazy big constant betting player"
 
 betRequest :: Object -> IO Int
-betRequest gameState = return 10
+betRequest gameState = return 10000
 
 showdown :: Object -> IO ()
 showdown gameState = return ()
